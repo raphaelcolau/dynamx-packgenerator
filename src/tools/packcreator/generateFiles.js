3d8ea92f3800d76bd83d640d3476d924f25948e2
Raphael C <48066106+raphaelcolau@users.noreply.github.com>
2023-07-13 14:59:15 +0200

    Create generateFiles.js

---
+++ src/tools/packcreator/generateFiles.js
@@ -0,0 +1,8 @@
+//TODO: Generate pack_info.json
+//TODO: Generate lang files
+
+export default function generatePack(files, pack) {
+    console.log("Generating pack...");
+    // console.log(files);
+    console.log(pack);
+}
