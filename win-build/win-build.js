04cc5ce94e130a40467cab1d0d49dc2a64286029
Raphael C <48066106+raphaelcolau@users.noreply.github.com>
2023-07-15 18:41:19 +0200

    Working to add an icon on Windows

---
+++ win-build/win-build.js
@@ -0,0 +1,19 @@
+const exe = require('@angablue/exe');
+
+console.log('Building...');
+const build = exe({
+    entry: '../src/index.js',
+    out: '../dist/PackManager.exe',
+    pkg: ['-C', 'GZip'], // Specify extra pkg arguments
+    version: '2.4.2',
+    target: 'latest-win-x64',
+    icon: './icon.ico', // Application icons must be in .ico format
+    properties: {
+        FileDescription: 'Pack Manager for DynamX',
+        ProductName: 'DynamX Pack Manager',
+        LegalCopyright: 'Raphael Colau https://raphael.colau.fr',
+        OriginalFilename: 'PackageManager.exe'
+    }
+});
+
+build.then(() => console.log('Build completed!'));
