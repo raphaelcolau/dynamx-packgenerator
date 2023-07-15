ca53509552cc4342b971e9e925c378f738845d36
Raphael C <48066106+raphaelcolau@users.noreply.github.com>
2023-07-15 18:43:44 +0200

    wip windows icon

--- win-build/win-build.js
+++ win-build/win-build.js
@@ -2,12 +2,12 @@ const exe = require('@angablue/exe');
 
 console.log('Building...');
 const build = exe({
-    entry: '../src/index.js',
-    out: '../dist/PackManager.exe',
+    entry: './src/index.js',
+    out: './dist/PackManager.exe',
     pkg: ['-C', 'GZip'], // Specify extra pkg arguments
     version: '2.4.2',
     target: 'latest-win-x64',
-    icon: './icon.ico', // Application icons must be in .ico format
+    icon: './win-build/assets/icon.ico', // Application icons must be in .ico format
     properties: {
         FileDescription: 'Pack Manager for DynamX',
         ProductName: 'DynamX Pack Manager',
