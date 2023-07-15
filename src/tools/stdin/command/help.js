dcfc9ab5f2427a224f621239651d6249000f1c37
Raphael C <48066106+raphaelcolau@users.noreply.github.com>
2023-07-15 11:44:00 +0200

    fix color

--- src/tools/stdin/command/help.js
+++ src/tools/stdin/command/help.js
@@ -4,5 +4,5 @@ export function help() {
     console.log(chalk.blue('/help') + ' - Show this help');
     console.log(chalk.blue('/pack') + ' - help for pack command');
     console.log(chalk.blue('/clear') + ' - clear build folder');
-    console.log(chalk.blue('/list <type>' + " - display all parserd files of a type"));
+    console.log(chalk.blue('/list <type>') + " - display all parserd files of a type");
 }
