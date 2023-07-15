825a55415ee009bae975dee5a452a62f08e95be4
Raphael C <48066106+raphaelcolau@users.noreply.github.com>
2023-07-15 17:31:18 +0200

    await is now in a async function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { detector } from "./tools/detector/detector.js";
 import { stdinListener } from "./tools/stdin/stdin.js";
 
-// Detect all Packs
-const files = await detector();
+async function main() {
+    // Detect all Packs
+    const files = await detector();
+    
+    // Start stdin listener
+    stdinListener(files);
+}
 
-// Start stdin listener
-stdinListener(files);
+main();
