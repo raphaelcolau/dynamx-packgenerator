0aae936acc8afb492869e708b80ce5e7dd0a7dee
Raphael C <48066106+raphaelcolau@users.noreply.github.com>
2023-07-15 11:29:35 +0200

    Create help.js
    refactoring and color

---
+++ src/tools/stdin/command/help.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+export function help() {
+    console.log(chalk.blue('/help') + ' - Show this help');
+    console.log(chalk.blue('/pack') + ' - help for pack command');
+    console.log(chalk.blue('/clear') + ' - clear build folder');
+    console.log(chalk.blue('/list <type>' + " - display all parserd files of a type"));
+}
