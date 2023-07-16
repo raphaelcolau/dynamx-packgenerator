8478bc7c3bc2c944cbdc7e42c22a6f83108ec261
Raphael C <48066106+raphaelcolau@users.noreply.github.com>
2023-07-16 14:29:25 +0200

    Close the program after one minute idle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const stdinListener = require("./tools/stdin/stdin.js").stdinListener;
+const chalk = require("chalk");
 
 async function main() {
     stdinListener();
 }
+
+//Kill the process after been idle for 1 minutes
+setTimeout(() => {
+    console.log(chalk.red("Process killed: ") + "Process has been idle for 1 minute.");
+    console.log("Bye, bye! (Dartcher)" + chalk.magenta(" <3"));
+    process.exit();
+}, 60000);
 
 main();
